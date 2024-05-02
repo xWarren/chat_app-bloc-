a65377e1512d06d339997c29441d0f61c687bc10
Warren Virgines <xwarrenvirgines@gmail.com>
2024-05-02 20:35:43 +0800

    added colors and assets

---
+++ lib/presentation/constants/assets.dart
@@ -0,0 +1,7 @@
+class Assets {
+
+  static const onboarding1 = "assets/images/onboarding_1.png";
+  static const onboarding3 = "assets/images/onboarding_2.png";
+  static const onboarding2 = "assets/images/onboarding_3.png";
+
+}
---
+++ lib/presentation/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+
+  static const Color primaryColor = Color(0xFFE6AA3F);
+  static const Color black = Color(0xFF252525);
+  static const Color grey = Color(0xFFE3E3E3);
+  static const Color grey100 = Color(0xFF858585);
+}
